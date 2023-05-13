e8650e741fab32ccfef6e4258179033a9f560144
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-05-13 21:14:17 +0400

    style: add animations

---
+++ src/FirstPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./FirstPage.css";
+
+const FirstPage = () => {
+  <div className="first-page">
+    <div className="sun"></div>
+    <div className="planet mercury"></div>
+    <div className="planet venus"></div>
+    <div className="planet earth"></div>
+    <div className="planet mars"></div>
+    <div className="planet jupiter"></div>
+    <div className="planet saturn"></div>
+    <div className="planet uranus"></div>
+    <div className="planet neptune"></div>
+    <div className="planet pluto"></div>
+  </div>;
+};
+export default FirstPage;
