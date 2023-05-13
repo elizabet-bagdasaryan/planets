e498dc597dc5153f522ca3138e0fd2ae9f85c2de
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-05-13 22:40:31 +0400

    feat: add firstpage component with animation

--- src/FirstPage.jsx
+++ src/FirstPage.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import "./FirstPage.css";
 
 const FirstPage = () => {
-  <div className="first-page">
-    <div className="sun"></div>
-    <div className="planet mercury"></div>
-    <div className="planet venus"></div>
-    <div className="planet earth"></div>
-    <div className="planet mars"></div>
-    <div className="planet jupiter"></div>
-    <div className="planet saturn"></div>
-    <div className="planet uranus"></div>
-    <div className="planet neptune"></div>
-    <div className="planet pluto"></div>
-  </div>;
+  return (
+    <div className="first-page">
+      <div className="sun"></div>
+      <div className="planets mercury"></div>
+      <div className="planets venus"></div>
+      <div className="planets earth"></div>
+      <div className="planets mars"></div>
+      <div className="planets jupiter"></div>
+      <div className="planets saturn"></div>
+      <div className="planets uranus"></div>
+      <div className="planets neptune"></div>
+      <div className="planets pluto"></div>
+    </div>
+  );
 };
+
 export default FirstPage;
