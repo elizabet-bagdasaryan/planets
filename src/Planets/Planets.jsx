705add35f60b8b084814feebe9243eeb775a393d
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-05-14 20:35:26 +0400

    refactor: code organizing

---
+++ src/Planets/Planets.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import "./Planets.css";
+import data from "../../data.json";
+import NavBar from "../NavBar/NavBar";
+import Overview from "../Buttons/Overview";
+import Structure from "../Buttons/Structure";
+import Geology from "../Buttons/Geology";
+import Footer from "../Footer/Footer";
+import { useMediaQuery } from "react-responsive";
+import FirstPage from "../FirstPage/FirstPage";
+
+function Planets() {
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+  const [activeSection, setActiveSection] = useState("overview");
+  const [isClicked, setIsClicked] = useState(false);
+
+  const isSmallScreen = useMediaQuery({ maxWidth: 768 });
+
+  const handleChoice = () => {
+    setIsClicked((prevState) => !prevState);
+  };
+  const handlePlanetClick = (planet) => {
+    setSelectedPlanet(planet);
+    setActiveSection("overview");
+  };
+
+  const handleSectionClick = (section) => {
+    setActiveSection(section);
+  };
+
+  const getButtonStyle = (section) => {
+    if (activeSection === section && selectedPlanet) {
+      return {
+        borderBottomColor: selectedPlanet.color,
+      };
+    }
+    return {};
+  };
+
+  const getBigButtonStyle = (section) => {
+    if (activeSection === section && selectedPlanet) {
+      return {
+        backgroundColor: selectedPlanet.color,
+      };
+    }
+    return {};
+  };
+
+  const planetNames = data.map((planet) => planet.name);
+
+  return (
+    <div className="main-section">
+      <NavBar
+        planetNames={planetNames}
+        handlePlanetClick={handlePlanetClick}
+        data={data}
+        handleChoice={handleChoice}
+        isClicked={isClicked}
+      />
+
+      <div
+        className={`planet-details ${
+          isSmallScreen && !isClicked ? "small-screen-style" : ""
+        }`}
+      >
+        {selectedPlanet ? (
+          <>
+            <div className="section-buttons">
+              <button
+                className={activeSection === "overview" ? "active" : ""}
+                style={getButtonStyle("overview")}
+                onClick={() => handleSectionClick("overview")}
+              >
+                Overview
+              </button>
+              <button
+                className={activeSection === "structure" ? "active" : ""}
+                style={getButtonStyle("structure")}
+                onClick={() => handleSectionClick("structure")}
+              >
+                Structure
+              </button>
+              <button
+                className={activeSection === "geology" ? "active" : ""}
+                style={getButtonStyle("geology")}
+                onClick={() => handleSectionClick("geology")}
+              >
+                Geology
+              </button>
+            </div>
+            <hr className="hr-buttons" />
+            {activeSection === "overview" && (
+              <Overview planet={selectedPlanet} />
+            )}{" "}
+            <div className="section-buttons-desk">
+              <button
+                className={activeSection === "overview" ? "active" : ""}
+                style={getBigButtonStyle("overview")}
+                onClick={() => handleSectionClick("overview")}
+              >
+                <p>01 </p> <p>Overview</p>
+              </button>
+              <button
+                className={activeSection === "structure" ? "active" : ""}
+                style={getBigButtonStyle("structure")}
+                onClick={() => handleSectionClick("structure")}
+              >
+                <p>02 </p> <p>Internal Structure</p>
+              </button>
+              <button
+                className={activeSection === "geology" ? "active" : ""}
+                style={getBigButtonStyle("geology")}
+                onClick={() => handleSectionClick("geology")}
+              >
+                <p>03 </p> <p>Surface Geology</p>
+              </button>
+            </div>
+            {activeSection === "structure" && (
+              <Structure planet={selectedPlanet} />
+            )}
+            {activeSection === "geology" && <Geology planet={selectedPlanet} />}
+          </>
+        ) : (
+          <FirstPage />
+        )}
+        {selectedPlanet && <Footer planet={selectedPlanet} />}
+      </div>
+    </div>
+  );
+}
+
+export default Planets;
